c188fe30cab23aefae32f687f29fb4564fa4371e
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-02-08 13:26:55 +0200

    slider done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-
+$(function () {
+    $('.carousel__inner').slick({
+            slidesToShow: 3,
+            arrows: false,
+            dots: true,
+            // centerMode: true
+    });
 });
